55447203c6181dba1e733efe3368b67e488657ec
sd7456852 <43368117+sd7456852@users.noreply.github.com>
2022-04-19 01:13:19 +0800

    0419-1

--- resources/views/Bootstrap index.blade.php
+++ resources/views/Bootstrap index.blade.php
@@ -3,10 +3,6 @@
 @section('title')
 @endsection
 
-
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 @section('css')
     <style>
@@ -55,7 +51,6 @@
         nav a {
             margin: 0 0 0 16px;
         }
-
 
         nav .dropdown-content {
             width: 192px;
@@ -179,7 +174,7 @@
             background-color: white;
             position: absolute;
             left: 80%;
-            top: 50%;
+            top: 25%;
             transform: translate(-50%, -50%);
             border-radius: 20px;
         }
@@ -196,11 +191,11 @@
 @endsection
 
 @section('nav')
-    <div class="container-xxl d-flex px-5">
-        <div class="col d-flex align-items-center">
-            <img src="/logo.svg" alt="" width="106px" height="60px">
+    <div class="container-xxl d-flex">
+        <div class="col d-flex align-items-center px-5">
+            <img src={{asset('./img/logo.svg')}} alt="" width="106px" height="60px">
         </div>
-        <div class="col box1 d-flex align-items-center justify-content-end">
+        <div class="col box1 d-flex align-items-center justify-content-end px-5 ">
             <a href="">Blog</a>
             <a href="">Portfolio</a>
             <a href="">About</a>
@@ -208,7 +203,7 @@
             <div class="box2">
                 <a href="http://127.0.0.1:8000/page1"><i class="bi bi-cart-fill"></i></a>
                 <a href="http://127.0.0.1:8000/login"><i class="bi bi-person-circle"></i>
-                    <div class="dropdown-content"><a href="">Login</a></div>
+                    {{-- <div class="dropdown-content"><a href="">Login</a></div> --}}
                 </a>
             </div>
         </div>
@@ -216,7 +211,7 @@
 @endsection
 @section('main')
     <section id="banner">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="swiper mySwiper">
                     <div class="swiper-wrapper">
@@ -247,7 +242,7 @@
             <div class="row d-flex text-center">
                 <div class="card" style="width: 33.33%;">
                     <div class="img">
-                        <img src="/pulse.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/pulse.png')}} class="card-img-top" alt="...">
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">Shooting Stars</h5>
@@ -259,7 +254,7 @@
                 </div>
                 <div class="card" style="width: 33.33%;">
                     <div class="img">
-                        <img src="/scissors.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/scissors.png')}} class="card-img-top" alt="...">
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">The Catalyzer</h5>
@@ -271,7 +266,7 @@
                 </div>
                 <div class="card" style="width: 33.33%;">
                     <div class="img">
-                        <img src="/user.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/user.png')}} class="card-img-top" alt="...">
                     </div>
                     <div class="card-body">
                         <h5 class="card-title">Neptune</h5>
@@ -301,14 +296,14 @@
             </div>
             <div class="row">
                 <div class="row" style="width: 50%;">
-                    <img src="/500x300.png" alt="" width="50%" height="178" style="border: 3px solid;color: white;">
-                    <img src="/500x300.png" alt="" width="50%" height="178" style="border: 3px solid;color: white;">
-                    <img src="/500x300.png" alt="" width="100%" height="365" style="border: 3px solid;color: white;">
+                    <img src={{asset('./img/500x300.png')}} alt="" width="50%" height="178" style="border: 3px solid;color: white;">
+                    <img src={{asset('./img/500x300.png')}} alt="" width="50%" height="178" style="border: 3px solid;color: white;">
+                    <img src={{asset('./img/500x300.png')}} alt="" width="100%" height="365" style="border: 3px solid;color: white;">
                 </div>
                 <div class="row" style="width: 50%;">
-                    <img src="/500x300.png" alt="" width="100%" height="365" style="border: 3px solid;color: white;">
-                    <img src="/500x300.png" alt="" width="50%" height="178" style="border: 3px solid;color: white;">
-                    <img src="/500x300.png" alt="" width="50%" height="178" style="border: 3px solid;color: white;">
+                    <img src={{asset('./img/500x300.png')}} alt="" width="100%" height="365" style="border: 3px solid;color: white;">
+                    <img src={{asset('./img/500x300.png')}} alt="" width="50%" height="178" style="border: 3px solid;color: white;">
+                    <img src={{asset('./img/500x300.png')}} alt="" width="50%" height="178" style="border: 3px solid;color: white;">
                 </div>
             </div>
         </div>
@@ -407,7 +402,7 @@
             <div class="row">
                 <div class="col-12 col-md-3">
                     <div class="card">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>SUBTITLE</h3>
                             <h2 class="card-title">Chichen Itza</h2>
@@ -419,7 +414,7 @@
                 </div>
                 <div class="col-12 col-md-3">
                     <div class="card">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>SUBTITLE</h3>
                             <h2 class="card-title">Chichen Itza</h2>
@@ -431,7 +426,7 @@
                 </div>
                 <div class="col-12 col-md-3">
                     <div class="card">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>SUBTITLE</h3>
                             <h2 class="card-title">Chichen Itza</h2>
@@ -443,7 +438,7 @@
                 </div>
                 <div class="col-12 col-md-3">
                     <div class="card">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>SUBTITLE</h3>
                             <h2 class="card-title">Chichen Itza</h2>
@@ -462,7 +457,7 @@
             <div class="row justify-content-center border-bottom">
                 <div class="row-4">
                     <div class="bg d-flex justify-content-center align-self-center">
-                        <img src="/activity.svg" alt="" width="30px" height="30px">
+                        <img src={{asset('./img/activity.svg')}} alt="" width="30px" height="30px">
                     </div>
                 </div>
                 <div class="col-12 col-md-6 ">
@@ -475,7 +470,7 @@
             <div class="row flex-row-reverse justify-content-center border-bottom">
                 <div class="row-4">
                     <div class="bg d-flex justify-content-center align-self-center">
-                        <img src="/activity.svg" alt="" width="30px" height="30px">
+                        <img src={{asset('./img/activity.svg')}} alt="" width="30px" height="30px">
                     </div>
                 </div>
                 <div class="col-12 col-md-6">
@@ -488,7 +483,7 @@
             <div class="row justify-content-center">
                 <div class="row-4">
                     <div class="bg d-flex justify-content-center align-self-center border-bottom">
-                        <img src="/activity.svg" alt="" width="30px" height="30px">
+                        <img src={{asset('./img/activity.svg')}} alt="" width="30px" height="30px">
                     </div>
                 </div>
                 <div class="col-12 col-md-6">
@@ -507,7 +502,7 @@
         <div class="container-xxl">
             <div class="row d-flex flex-column flex-md-row">
                 <div class="col-12 col-md-6 h-auto ">
-                    <img src="/400x400.png" alt="" class="merch_pic h-auto">
+                    <img src={{asset('./img/400x400.png')}} alt="" class="merch_pic h-auto">
                 </div>
                 <div class="col-12 col-md-6 pt-4 pb-4 pe-0 ps-5">
                     <h2 class="suptitle">BRAND NAME</h2>
@@ -589,7 +584,7 @@
             <div class="row">
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -599,7 +594,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -609,7 +604,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -619,7 +614,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -629,7 +624,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -639,7 +634,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -649,7 +644,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -659,7 +654,7 @@
                 </div>
                 <div class="col-6 col-md-3">
                     <div class="card p-1">
-                        <img src="/720x400.png" class="card-img-top" alt="...">
+                        <img src={{asset('./img/720x400.png')}} class="card-img-top" alt="...">
                         <div class="card-body">
                             <h3>CATEGORY</h3>
                             <h2 class="card-title">The Catalyzer</h2>
@@ -703,10 +698,10 @@
 @endsection
 @section('footer')
     <section id="sitemap">
-        <div class="container-xxl">
-            <div class="row justify-content-around ">
-                <div class="col-2">
-                    <img src="/logo.svg" alt="" width="100px" height="100px">
+        <div class="container-fluid">
+            <div class="row ">
+                <div class="col">
+                    <img src={{asset('./img/logo.svg')}} alt="" width="100px" height="100px">
                     <span>數位方塊</span>
                     <p>Air plant banjo lyft occupy retro adaptogen indego</p>
                 </div>
@@ -749,16 +744,16 @@
                 <p>© 2020 Tailblocks — @knyttneve</p>
                 <span>
                     <a href="">
-                        <img src="/facebook.svg" alt="">
+                        <img src={{asset('./img/facebook.svg') }} alt="">
                     </a>
                     <a href="">
-                        <img src="/twitter.svg" alt="">
+                        <img src={{asset('./img/twitter.svg') }} alt="">
                     </a>
                     <a href="">
-                        <img src="/instagram.svg" alt="">
+                        <img src={{asset('./img/instagram.svg') }} alt="">
                     </a>
                     <a href="">
-                        <img src="/linkedin.png" alt="">
+                        <img src={{asset('./img/linkedin.png') }} alt="">
                     </a>
                 </span>
             </div>
